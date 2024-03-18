15df9036504a613ef40145064218cd5ea4f9d1d9
Tarek Aloui <taloui@college.harvard.edu>
2024-03-18 04:33:51 -0400

    added / in /tmp/tmp_file

--- src/main.py
+++ src/main.py
@@ -9,7 +9,9 @@ app = FastAPI()
 @app.post("/extract-info/", response_model=LabReportInfo)
 async def extract_info_endpoint(file: UploadFile = File(...)):
     # Save the uploaded file temporarily
-    temp_file_path = f"tmp/temp_{file.filename}"
+    if not os.path.exists("tmp"):
+        os.makedirs("tmp")
+    temp_file_path = f"/tmp/temp_{file.filename}"
     with open(temp_file_path, "wb") as buffer:
         buffer.write(await file.read())
 
